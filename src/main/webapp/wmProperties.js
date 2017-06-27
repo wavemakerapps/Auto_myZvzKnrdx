74f623d4ec4cd5f0e4ebf9bd4b71af32ea2ca7a4
root <root@7735277998e8>
2017-06-27 00:45:04 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_myZvzKnrdx",
+  "homePage" : "Main",
+  "name" : "Auto_myZvzKnrdx",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
